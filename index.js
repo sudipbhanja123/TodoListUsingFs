77843c72cbb6652a70aeacb70ee26e023af2f3b4
sudipbhanja123 <sudipbhanja5g@gmail.com>
2024-06-28 22:19:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      return res.render("index", { files: [] });
+    }
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
+  });
+});
+
+app.post("/create", (req, res) => {
+  const title = req.body.title.split(" ").join("");
+  const details = req.body.details;
+
+  fs.writeFile(`./files/${title}.txt`, details, (err) => {
+    if (err) {
+      return res.status(500).send("Error saving the task.");
+    }
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
